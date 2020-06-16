649313505720c1fd544d8468c30834e6afe37dba
ATon <aton666@163.com>
2020-06-16 16:13:46 +0800

    增加了校验位处理的测试

--- src/test/java/com/sxm/agriculture/mytest.java
+++ src/test/java/com/sxm/agriculture/mytest.java
@@ -2,6 +2,9 @@ package com.sxm.agriculture;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.net.*;
+
 /**
  * @author 780013490@qq.com  孙小明
  * @version 1.0
@@ -21,8 +24,8 @@ public class mytest {
     @Test
     public void test2() {
 
-        String s = "FFAA01010101010101010101010101010101010101010101010101010101EEBB";
-        if (s.startsWith("FFAA") && s.endsWith("EEBB")&&s.length()==64) {
+        String s = "FFAA010101010101010101010101010101010101010101010101010101010000EEBB";
+        if (s.startsWith("FFAA") && s.endsWith("0000EEBB") && s.length() == 68) {
             System.out.println(true);
         }
         String substring = s.substring(60, 64);
@@ -36,13 +39,22 @@ public class mytest {
         int integer = Integer.valueOf(s, 16);
         System.out.println(integer);
 //        integer = Integer.valueOf(s, 16)/10;
-        float i = (float)integer / 10;
+        float i = (float) integer / 10;
         System.out.println(i);
 
     }
 
     @Test
-    public void teste() {
+    public void teste() throws IOException {
+        String str = "FFAA01010101010101010101010101010101010101010101010101010101EEBB";
+
+        DatagramSocket socket = new DatagramSocket();
+
+        DatagramPacket packet = new DatagramPacket(str.getBytes(), str.getBytes().length, InetAddress.getByName("47.102.218.114"), 18080);
+
+        socket.send(packet);
+        socket.close();
+
 
     }
 
